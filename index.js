8e037967ac4ddc72e5d1eab7c45e31bee90bfd98
MarcAntoineIseg <83115828+MarcAntoineIseg@users.noreply.github.com>
2025-06-28 14:15:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
